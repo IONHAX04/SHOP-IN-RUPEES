4f8c1e9f0ad336095081a4e25c252b89fcaf4aeb
IONHAX04 <thirukumara04@gmail.com>
2024-11-12 09:26:22 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'shopinrupees',
+  webDir: 'dist'
+};
+
+export default config;
